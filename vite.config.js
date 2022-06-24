86964d8dd9478db9a69e26330eff61a9120ac3f0
wesleywil <wesleywilsonti@gmail.com>
2022-06-24 16:06:42 -0300

    added tauri for the desktop app build and development

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,16 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  clearScreen:false,
+  plugins: [react()],
+  server:{
+    port:3000,
+    strictPort:true,
+  },
+  envPrefix:['VITE_', 'TAURI_'],
+  build:{
+    target:['es2021', 'chrome97','safari13'],
+    minify:!process.env.TAURI_DEBUG ? 'esbuild':false,
+    sourcemap: !!process.env.TAURI_DEBUG,
+  },
 })
