809d23ca82fe9409ca8002bba45041b8fe9cdeee
wesleywil <wesleywilsonti@gmail.com>
2022-07-11 12:44:05 -0300

    restart project and  installed tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,19 @@
-import React from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 //Pages
-import Homepage from './pages/homepage/homepage.component'
+import Homepage from "./pages/homepage/homepage.component";
 
-import './App.css'
+import "./App.css";
 
 function App() {
-
-
   return (
     <BrowserRouter>
       <Routes>
-        <Route exact='true' path='*' element={<Homepage/>} />
+        <Route exact="true" path="/" element={<Homepage />} />
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
