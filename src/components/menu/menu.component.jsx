cffdf0518efef64e1a3dc4e1ab5a9da6eb16327d
wesleywil <wesleywilsonti@gmail.com>
2023-02-21 20:19:02 -0300

    updated menu component by adding links to the homepage and animes page

--- src/components/menu/menu.component.jsx
+++ src/components/menu/menu.component.jsx
@@ -1,13 +1,20 @@
+import { Link } from "react-router-dom";
 import { FaHome, FaList } from "react-icons/fa";
 
 const Menu = ()=>{
     return(
 <div className="flex justify-center gap-2 text-3xl mt-2">
         <div className="tooltip tooltip-bottom" data-tip="Homepage">
-          <button className="bg-red-600 p-2 rounded"><FaHome /></button>
+        <div  className="bg-red-600 p-2 rounded">
+          <Link to="/"><FaHome /></Link> 
+          </div>
+          
         </div>
         <div className="tooltip tooltip-bottom" data-tip="Animes">
-          <button className="bg-blue-600 p-2 rounded"><FaList /></button> 
+          <div  className="bg-blue-600 p-2 rounded">
+          <Link to="/animes"><FaList /></Link> 
+          </div>
+          
         </div>
         <div className="tooltip tooltip-bottom" data-tip="Mangas">
           <button className="bg-green-600 p-2 rounded"><FaList /></button>
