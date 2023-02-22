189c2f6a2bff6f4d9d331ed9a64092752b20341a
wesleywil <wesleywilsonti@gmail.com>
2023-02-22 13:52:52 -0300

    updated App.css and layout component by fixing the background size when window resize

--- src/components/layout/layout.component.jsx
+++ src/components/layout/layout.component.jsx
@@ -2,7 +2,7 @@ import Menu from "../../components/menu/menu.component";
 import SearchBar2 from "../../components/search_bar_2/search_bar_2.component";
 const Layout = (props) => {
   return (
-    <div className="p-2 h-screen border text-white">
+    <div className="p-2 min-h-screen border text-white">
       <SearchBar2 />
       <Menu />
       <div className="flex flex-col items-center mt-4">
