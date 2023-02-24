0e2b8161b2aa2704c7d5d7654d991eb6496738eb
wesleywil <wesleywilsonti@gmail.com>
2023-02-24 14:06:18 -0300

    updated utilsSlice by adding the isAnime state and set_is_anime reducer

--- src/redux/utils/utils.jsx
+++ src/redux/utils/utils.jsx
@@ -4,13 +4,19 @@ import { createSlice} from "@reduxjs/toolkit";
 
 const initialState = {
     cardInDB:false,
+    isAnime:true,
 };
 
 export const utilsSlice = createSlice({
     name:"utils",
     initialState,
     reducers:{
+        set_is_anime:(state, action)=>{
+            state.isAnime = action.payload;
+        }
     }
 })
+
+export const { set_is_anime } = utilsSlice.actions;
 
 export default utilsSlice.reducer;
