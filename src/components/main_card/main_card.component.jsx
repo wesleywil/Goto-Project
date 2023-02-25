08b6dcd9b16654d71da9b46655cd3d2d0248e561
wesleywil <wesleywilsonti@gmail.com>
2023-02-25 14:23:28 -0300

    fixed logic of the background image that was not verifying if the card info was from database or from the api response

--- src/components/main_card/main_card.component.jsx
+++ src/components/main_card/main_card.component.jsx
@@ -1,13 +1,16 @@
+import { useSelector } from "react-redux";
+
 import MainCardContent from "../main_card_content/main_card_content.component";
 
-const MainCard = ({info}) => {  
+const MainCard = ({info}) => {
+  const cardInDB = useSelector((state)=> state.utils.cardInDB);
     return (
       <div
         className=" p-2 border rounded-xl"
         style={{
           width: "220px",
           height: "270px",
-          backgroundImage: "url"+"("+`${info.images.webp.image_url}`+")",
+          backgroundImage: "url"+"("+`${info?cardInDB?info.image:info.images.webp.image_url:"https://dummyimage.com/220x270"}`+")",
         }}
       >
         <div className="w-full h-full p-2 flex flex-col items-center justify-between bg-black/40 border border-red-600 rounded-xl">
