caaf1b50e294c358364b27306dbda8b0fecb7e7b
wesleywil <wesleywilsonti@gmail.com>
2023-02-25 14:26:10 -0300

    updated main_card_content by spliting in to two other components to split the add logic from the update logic

---
+++ src/components/card_content_update/card_content_update.component.jsx
@@ -0,0 +1,116 @@
+import { useRef, useState } from "react";
+import { useSelector } from "react-redux";
+
+import { updateAnime } from "../../server/db_functions_animes";
+import { updateManga } from "../../server/db_functions_mangas";
+
+const CardContentUpdate = ({ item }) => {
+  const link = useRef(null);
+  const status = useRef(null);
+  const rate = useRef(null);
+  const [hidden, setHidden] = useState(true);
+  const isAnime = useSelector((state) => state.utils.isAnime);
+
+  const handleHidden = () => {
+    setHidden(!hidden);
+  };
+
+  const handleUpdateData = () => {
+    const data = {
+      title: item.title,
+      image: item.image,
+      description: item.description,
+      link: link.current.value,
+      status: status.current.value,
+      rate: rate.current.value,
+      review: item.review,
+      id: item.id,
+    };
+
+    if (isAnime) {
+      console.log("Anime data=> ", data);
+      // updateAnime(data).then((res)=>{
+      //   console.log('Anime updated! ', res);
+      // })
+    } else {
+      console.log("Manga data=> ", data);
+      // updateManga(data).then((res)=>{
+      //   console.log('Manga updated! ', res);
+      // })
+    }
+  };
+  const handleGoto = (link) => {
+    window.open(link, "_blank");
+  };
+  return (
+    <>
+      <div className="flex flex-col items-center justify-center">
+        <h1 className="text-xl">
+          {item
+            ? item.title.length > 15
+              ? item.title.slice(0, 15) + "..."
+              : item.title
+            : "Title's goes here"}
+        </h1>
+        {/* <h2>(2002)</h2> */}
+      </div>
+      <div className="w-full flex flex-col gap-2">
+        <input
+          className="w-full text-black text-xl rounded"
+          defaultValue={item ? item.link : ""}
+          ref={link}
+          type="text"
+          placeholder="Link in where you watch/read"
+        />
+        <select
+          className="w-full text-black text-xl rounded"
+          defaultValue={item ? item.status : "None"}
+          ref={status}
+        >
+          <option value="None" disabled>
+            None
+          </option>
+          <option value="Reading/Watching">Reading/Watching</option>
+          <option value="Read/Watched">Read/Watched</option>
+          <option value="Dropped">Dropped</option>
+        </select>
+        <input
+          type="range"
+          min="0"
+          max="10"
+          defaultValue={item ? item.rate : 5}
+          ref={rate}
+          className="range range-warning"
+        />
+        <div className="flex justify-center gap-3 bg-black/50 backdrop-blur-sm rounded">
+          <button
+            onClick={() => handleGoto(link)}
+            className="font-bold text-blue-400 hover:text-blue-600 hover:underline"
+          >
+            GOTO
+          </button>
+          <button
+            onClick={() => handleUpdateData()}
+            className="font-bold text-green-400 hover:text-green-600 hover:underline"
+          >
+            Update
+          </button>
+          <button
+            onClick={() => handleHidden()}
+            className="font-bold text-red-400 hover:text-red-600 hover:underline"
+          >
+            Delete
+          </button>
+        </div>
+        <div className={hidden?"hidden":""+"w-full flex flex-col items-center bg-black/50 backdrop-blur-sm rounded"}>
+          <h1>Delete?</h1>
+          <div className="flex gap-2 font-bold">
+            <button className="text-red-400 hover:text-red-600">Yes</button>/<button onClick={()=>setHidden(!hidden)} className="text-green-400 hover:text-green-600">No</button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default CardContentUpdate;
--- src/components/main_card_content/main_card_content.component.jsx
+++ src/components/main_card_content/main_card_content.component.jsx
@@ -1,80 +1,19 @@
-import { useEffect } from "react";
 import { useSelector } from "react-redux";
 
-import { createAnime } from "../../server/db_functions_animes";
-import { createManga } from "../../server/db_functions_mangas";
+import CardContentAdd from "../card_content_add/card_content_add.component";
+import CardContentUpdate from "../card_content_update/card_content_update.component";
 
 const MainCardContent = ({item}) => {
   const cardInDB = useSelector((state) => state.utils.cardInDB);
-  const isAnime = useSelector((state)=> state.utils.isAnime);
 
-  useEffect(()=>{
-    console.log('IS ANIME=> ', isAnime);
-  },[isAnime])
 
-  const handleAddAnime = (itemData)=>{
-    const data = {
-      title: itemData.title,
-      image: itemData.images.webp.image_url,
-      description: itemData.synopsis,
-      link: "",
-      status: "Reading/Watching",
-      rate: 5,
-      review: "",
-    };
-
-    if (isAnime) {
-      console.log("Creating anime...");
-      createAnime(data).then((res) => {
-        //setMessage(res);
-        console.log(res);
-        // setTimeout(() => location.reload(), 4000);
-      });
-    } else {
-      console.log("Creating manga...");
-      createManga(data).then((res) => {
-        //setMessage(res);
-        console.log(res);
-        // setTimeout(() => location.reload(), 4000);
-      });
-    }
-  }
   if (!cardInDB) {
     return (
-      <>
-        <div className="flex flex-col items-center justify-center">
-          <h1 className="text-2xl">{item?item.title:"Anime's Name"}</h1>
-          <h2>{item? isAnime?item.aired.prop.from.year:item.published.prop.from.year:"No Year"}</h2>
-        </div>
-        <button onClick={()=>handleAddAnime(item)} className="w-full text-xl font-bold bg-red-600 hover:bg-red-800 rounded-xl">
-          Add
-        </button>
-      </>
+      <CardContentAdd item={item}/>
     );
   } else {
     return (
-      <>
-        <div className="flex flex-col items-center justify-center">
-          <h1 className="text-2xl">Anime's name</h1>
-          <h2>(2002)</h2>
-        </div>
-        <div className="w-full flex flex-col gap-2">
-          <select className="w-full text-black text-xl rounded">
-            <option value="0">Watching</option>
-            <option value="1">Watched</option>
-            <option value="2">Dropped</option>
-          </select>
-          <input
-            type="range"
-            min="0"
-            max="10"
-            className="range range-warning"
-          />
-          <button className="w-full text-xl font-bold bg-red-600 hover:bg-red-800 rounded-xl">
-          Update
-        </button>
-        </div>
-      </>
+      <CardContentUpdate item={item}/>
     );
   }
 };
