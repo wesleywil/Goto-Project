6e77ccb44cf85a0d2c332ae44905a2017c1ce40a
wesleywil <wesleywilsonti@gmail.com>
2023-02-26 12:42:48 -0300

    updated homepage by chaging some small details in the item_showcase component

--- src/components/item_showcase/item_showcase.component.jsx
+++ src/components/item_showcase/item_showcase.component.jsx
@@ -1,10 +1,8 @@
-const ItemShowcase = ({ qtd, name, color }) => {
+const ItemShowcase = ({ qtd, name }) => {
   return (
-    <div className={`flex flex-col items-center self-center justify-center text-6xl backdrop-blur-sm bg-${color}-600/20 border-2 border-${color}-600 p-4 rounded-full w-80 h-80`}>
+    <div className="flex flex-col items-center self-center justify-center text-6xl backdrop-blur-sm bg-purple-600/20 border-2 border-green-600 p-4 rounded-full w-80 h-80">
       {qtd}
-      <button className={`mt-2 bg-${color}-200 text-gray-800 font-semibold text-3xl px-1 rounded-xl`}>
-        {name}
-      </button>
+      <h1 className="text-xl text-white">{name}</h1>
     </div>
   );
 };
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,12 +1,16 @@
+import { useSelector } from "react-redux";
+
 import Layout from "../../components/layout/layout.component";
 import ItemShowcase from "../../components/item_showcase/item_showcase.component";
 
 const Homepage = () => {
+  const animes = useSelector((state)=> state.animes.animes);
+  const mangas = useSelector((state)=> state.mangas.mangas);
   return (
     <Layout name="Homepage">
       <div className="mt-4 flex justify-center gap-4">
-        <ItemShowcase qtd={300} name="Goto Animes" color={"blue"} />
-        <ItemShowcase qtd={2850} name="Goto Mangas" color={"green"} />
+        <ItemShowcase qtd={animes.length} name="Animes"/>
+        <ItemShowcase qtd={mangas.length} name="Mangas"/>
       </div>
     </Layout>
   );
