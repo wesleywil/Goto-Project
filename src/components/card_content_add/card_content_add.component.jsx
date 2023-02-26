aca4a067ff9bd6bdc79eadfceff52d8002d90b83
wesleywil <wesleywilsonti@gmail.com>
2023-02-26 13:57:43 -0300

    updated main_card component, card_content_add component by changing how I was setting the background image and year, now I'm using the useState and useEffect hooks

--- src/components/card_content_add/card_content_add.component.jsx
+++ src/components/card_content_add/card_content_add.component.jsx
@@ -1,55 +1,74 @@
-import { useEffect } from "react"
-import { useSelector } from "react-redux"
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
 
 import { createAnime } from "../../server/db_functions_animes";
 import { createManga } from "../../server/db_functions_mangas";
 
-const CardContentAdd = ({item})=>{
-    const isAnime = useSelector((state)=> state.utils.isAnime);
+const CardContentAdd = ({ item }) => {
+  const isAnime = useSelector((state) => state.utils.isAnime);
+  const cardInDB = useSelector((state) => state.utils.cardInDB);
+  const [year, setYear] = useState("No Year");
 
-    useEffect(()=>{
-        console.log('Is anime? ', isAnime);
-    },[isAnime])
-
-    const handleAddAnime = (itemData)=>{
-        const data = {
-          title: itemData.title,
-          image: itemData.images.webp.image_url,
-          description: itemData.synopsis,
-          link: "",
-          status: "Reading/Watching",
-          rate: 5,
-          review: "",
-        };
-    
-        if (isAnime) {
-          console.log("Creating anime...");
-          createAnime(data).then((res) => {
-            //setMessage(res);
-            console.log(res);
-            // setTimeout(() => location.reload(), 4000);
-          });
-        } else {
-          console.log("Creating manga...");
-          createManga(data).then((res) => {
-            //setMessage(res);
-            console.log(res);
-            // setTimeout(() => location.reload(), 4000);
-          });
-        }
+  useEffect(() => {
+    if (!cardInDB) {
+      if (isAnime) {
+        setYear(item.aired.prop.from.year);
+      } else {
+        setYear(item.published.prop.from.year);
       }
-      
-    return(
-        <>
-        <div className="flex flex-col items-center justify-center">
-          <h1 className="text-xl">{item?item.title.length > 15?item.title.slice(0, 15)+"...":item.title:"Title's goes here"}</h1>
-          <h2>{item? isAnime?item.aired.prop.from.year:item.published.prop.from.year:"No Year"}</h2>
-        </div>
-        <button onClick={()=>handleAddAnime(item)} className="w-full text-xl font-bold bg-red-600 hover:bg-red-800 rounded-xl">
-          Add
-        </button>
-      </> 
-    )
-}
+    } else {
+      setYear("No Year");
+    }
+  }, [item, year]);
+
+  const handleAddAnime = (itemData) => {
+    const data = {
+      title: itemData.title,
+      image: itemData.images.webp.image_url,
+      description: itemData.synopsis,
+      link: "",
+      status: "Reading/Watching",
+      rate: 5,
+      review: "",
+    };
+
+    if (isAnime) {
+      console.log("Creating anime...");
+      createAnime(data).then((res) => {
+        //setMessage(res);
+        console.log(res);
+        // setTimeout(() => location.reload(), 4000);
+      });
+    } else {
+      console.log("Creating manga...");
+      createManga(data).then((res) => {
+        //setMessage(res);
+        console.log(res);
+        // setTimeout(() => location.reload(), 4000);
+      });
+    }
+  };
+
+  return (
+    <>
+      <div className="flex flex-col items-center justify-center">
+        <h1 className="text-xl">
+          {item
+            ? item.title.length > 15
+              ? item.title.slice(0, 15) + "..."
+              : item.title
+            : "Title's goes here"}
+        </h1>
+        <h2>{year}</h2>
+      </div>
+      <button
+        onClick={() => handleAddAnime(item)}
+        className="w-full text-xl font-bold bg-red-600 hover:bg-red-800 rounded-xl"
+      >
+        Add
+      </button>
+    </>
+  );
+};
 
 export default CardContentAdd;
--- src/components/main_card/main_card.component.jsx
+++ src/components/main_card/main_card.component.jsx
@@ -1,16 +1,34 @@
+import { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
 
 import MainCardContent from "../main_card_content/main_card_content.component";
 
 const MainCard = ({info}) => {
   const cardInDB = useSelector((state)=> state.utils.cardInDB);
+  const [backgroundImage, setBackgroundImage] = useState("https://dummyimage.com/220x270");
+  
+  useEffect(()=>{
+    console.log('Main card')
+    if(info){
+      if(cardInDB){
+        setBackgroundImage(info.image)
+      }else{
+        try {
+          setBackgroundImage(info.images.webp.image_url)
+        } catch (error) {
+          console.log('ERROR IN THE IMAGE' + error);
+        }
+      }
+    }
+  },[backgroundImage, info])
+
     return (
       <div
         className=" p-2 border rounded-xl"
         style={{
           width: "220px",
           height: "270px",
-          backgroundImage: "url"+"("+`${info?cardInDB?info.image:info.images.webp.image_url:"https://dummyimage.com/220x270"}`+")",
+          backgroundImage: "url"+"("+`${backgroundImage}`+")",
         }}
       >
         <div className="w-full h-full p-2 flex flex-col items-center justify-between bg-black/40 border border-red-600 rounded-xl">
